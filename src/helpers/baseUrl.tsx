9e23fc5748ce36dc4080f0dfc47c63a8be86ddf4
kaltechCode <kaltechconsultancy@gmail.com>
2025-11-19 05:12:29 -0500

    resolve conflict

--- src/helpers/baseUrl.tsx
+++ src/helpers/baseUrl.tsx
@@ -2,11 +2,7 @@ export const baseUrl = () => {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000";
   } else if (process.env.NODE_ENV === "production") {
-<<<<<<< HEAD
-    return "https://blizservices.com";
-=======
     return "https://https://blizservices.com/";
->>>>>>> 46b3e6c45dcdbb37d696201ca908a3c237507b54
   } else {
     return "/";
   }
